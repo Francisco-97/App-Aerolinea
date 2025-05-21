c40e1ebfedfb229649a7f077272c0a86c36781a1
Juan Armando Lopez Mendez <119714695+Z-ARMLM@users.noreply.github.com>
2025-05-20 22:24:44 -0400

    Probando creacion de componentes con el genio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListaVuelosComponent } from './lista-vuelos/lista-vuelos.component';
 import { HomeComponent } from './home/home.component';
+import { PagosComponent } from './pagos/pagos.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'listaVuelo', component:ListaVuelosComponent }
+    { path: 'listaVuelo', component:ListaVuelosComponent }, 
+    { path: 'pagos', component:PagosComponent }
 ];
