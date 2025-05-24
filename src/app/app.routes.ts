0a67b0a1644ee158edf7fd6e7daaced152e12d75
Juan Armando Lopez Mendez <119714695+Z-ARMLM@users.noreply.github.com>
2025-05-23 23:27:02 -0400

    Ciertas modificaciones, comprobando colores....

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,20 @@ import { Routes } from '@angular/router';
 import { ListaVuelosComponent } from './lista-vuelos/lista-vuelos.component';
 import { HomeComponent } from './home/home.component';
 import { PagosComponent } from './pagos/pagos.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { ListaReservacionesComponent } from './lista-reservaciones/lista-reservaciones.component';
+import { SeleccionarAsientosComponent } from './seleccionar-asientos/seleccionar-asientos.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'listaVuelo', component:ListaVuelosComponent }, 
-    { path: 'pagos', component:PagosComponent }
+    { path: 'pagos', component:PagosComponent },
+    {path: 'registro', component:RegistroComponent},
+    {path: 'login', component:LoginComponent},
+    {path: 'reservaciones', component:ListaReservacionesComponent},
+    {path: 'asientos', component:SeleccionarAsientosComponent}
+
+
+
 ];
--- src/app/seleccionar-asientos/seleccionar-asientos.component.ts
+++ src/app/seleccionar-asientos/seleccionar-asientos.component.ts
@@ -6,6 +6,40 @@ import { Component } from '@angular/core';
   templateUrl: './seleccionar-asientos.component.html',
   styleUrl: './seleccionar-asientos.component.scss'
 })
+
 export class SeleccionarAsientosComponent {
 
 }
+
+const airplane = document.getElementById("airplane");
+const rows = 10;
+    const seatsPerRow = ['A', 'B', '', 'C', 'D']; // '' es pasillo
+
+    for (let i = 1; i <= rows; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+
+      seatsPerRow.forEach(letter => {
+        if (letter === '') {
+          const aisle = document.createElement("div");
+          aisle.classList.add("aisle");
+          row.appendChild(aisle);
+        } else {
+          const seat = document.createElement("button");
+          seat.classList.add("seat");
+          seat.textContent = `${i}${letter}`;
+          seat.dataset['seat'] = `${i}${letter}`;
+
+          // Evento al hacer clic
+          seat.addEventListener("click", () => {
+            if (!seat.classList.contains("occupied")) {
+              seat.classList.toggle("selected");
+            }
+          });
+
+          row.appendChild(seat);
+        }
+      });
+
+      airplane.appendChild(row);
+    }
